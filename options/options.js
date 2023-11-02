1622faacb6dd74f4cb1c89dfd6f5410700d3264a
Crowdbotics <team@crowdbotics.com>
2023-11-02 06:00:00 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "krazy-kraving-44284",
+  url: "https://krazy-kraving-44284.botics.co",
+  api: "https://krazy-kraving-44284.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
